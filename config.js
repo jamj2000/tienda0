260a381046498a2c052af1e512c301ea7a58f9b6
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2018-09-02 16:03:05 +0200

    Uso de depends_on en lugar de links en docker

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 module.exports = {
   ip         : process.env.HOST   || '0.0.0.0',
   port       : process.env.PORT   || 3000,
-  db_uri     : process.env.DB_URI || 'mongodb://mongo:27017/tienda'
+  db_uri     : process.env.DB_URI || 'mongodb://localhost:27017/tienda'
 };
